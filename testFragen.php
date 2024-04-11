3d0a142024c30f7d0b01849dd22e7d50be0d1ed6
Dahrno <daniel@czeguhn.com>
2024-04-11 20:44:22 +0200

    tests mit buttons

--- testFragen.php
+++ testFragen.php
@@ -203,7 +203,7 @@ $conn->close();
                                             </div>
                                         <?php else: ?>
                                             <div class="w3-center w3-row">
-                                                <div class="w3-animate-zoom w3-third w3-right-align">
+                                                <div class="w3-animate-zoom w3-third w3-right-align back-button">
                                                     <button class="w3-left w3-button w3-round-large w3-dark-gray w3-hide-small" style="width:20%" type="submit" name="zurueck">Zurück</button>
                                                     <button class="w3-left w3-button w3-round-large w3-dark-gray w3-hide-medium w3-hide-large" style="width:30%" type="submit" name="zurueck">Zurück</button>
                                                 </div>

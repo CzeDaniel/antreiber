543b92ea5f6a04deca58a9bfa116ace7732faa0a
Dahrno <daniel@czeguhn.com>
2024-04-11 20:13:29 +0200

    ssa

--- testFragen.php
+++ testFragen.php
@@ -201,7 +201,7 @@ $conn->close();
                                                 <button class="w3-right w3-button w3-round-large w3-dark-gray w3-hide-medium w3-hide-large" style="width:30%" type="submit" name="test_abgeben" onclick="gotoAnswer()">Abgabe</button>
                                             </div>
                                         <?php else: ?>
-                                            <div class="row w3-third">
+                                            <div class="row w3-third w3-center">
                                                 <div class="w3-modal-content w3-animate-zoom">
                                                     <button class="w3-left w3-button w3-round-large w3-dark-gray w3-hide-small" style="width:20%" type="submit" name="zurueck">Zurück</button>
                                                     <button class="w3-left w3-button w3-round-large w3-dark-gray w3-hide-medium w3-hide-large" style="width:30%" type="submit" name="zurueck">Zurück</button>
